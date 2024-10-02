165abceb707f98d42d252e00973e13a3bdc8093e
hariake <tosarv@gmail.com>
2024-10-02 19:01:08 +0300

    Expo setup

---
+++ app/splash.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const splash = () => {
+  return (
+    <View className="text-white bg-black justicy-center items-center">
+      <Text>Youll find all you need Here!</Text>
+    </View>
+  )
+}
+
+export default splash
